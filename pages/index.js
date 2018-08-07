079008fa7aa422140a2652fa3f17cbe3fcf99c6f
balazs4 <balazs4web@gmail.com>
2018-08-07 23:10:03 +0200

    feat: add some styling

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,48 @@ import fetch from 'isomorphic-unfetch';
 
 const Home = ({ streams }) => {
   return (
-    <div>
+    <div className="container">
       {streams.map(s => (
-        <a
-          href={s.url.replace(
-            'http://piserver:8080/mediabag/proxy/mediaklikk/',
-            'https://mediaklikk.now.sh/'
-          )}
-          target="_new"
-        >
-          <div key={s.id}>
-            <h1>{s.name}</h1>
-            <img src={s.icon} alt={s.id} style={{ height: '106px' }} />
-          </div>
-        </a>
+        <div key={s.id} className="stream">
+          <a
+            href={s.url.replace(
+              'http://piserver:8080/mediabag/proxy/mediaklikk/',
+              'https://mediaklikk.now.sh/'
+            )}
+            target="_new"
+          >
+            <div>
+              <img src={s.icon} alt={s.id} style={{ height: '96px' }} />
+              <h4>{s.name}</h4>
+            </div>
+          </a>
+        </div>
       ))}
+      <style jsx>{`
+        .container {
+          width: 1000px;
+          margin: auto;
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          justify-content: space-around;
+        }
+        .stream {
+          background: white;
+          width: 192px;
+          text-align: center;
+          padding: 32px;
+          border: 0.4px solid gray;
+          margin: 16px;
+        }
+
+        a {
+          color: gray;
+        }
+        img {
+          background: lightgray;
+        }
+      `}</style>
     </div>
   );
 };
